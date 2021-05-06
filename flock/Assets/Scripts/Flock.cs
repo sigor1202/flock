20b51c0e56a882cabaa9bf86aed7846778c2c88e
sigor1202 <sigor7829@gmail.com>
2021-05-06 19:20:39 -0300

    flock

---
+++ flock/Assets/Scripts/Flock.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    //variavel para fazer referencia ao script myManager
+    public FlockManager myManager;
+    //velocidade
+    float speed;
+    //boleado usado na verificação da delimitação
+    bool turning = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //randomiza a velocidade entre o vavlor minimo e maximo
+        speed = Random.Range(myManager.minSpeed, myManager.maxSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //limita a area em que os peixes irão se mover
+        Bounds b = new Bounds(myManager.transform.position, myManager.swinLimits * 2);
+        //se sair da area turning sera verdadeiro
+        if (!b.Contains(transform.position))
+        {
+            turning = true;
+        }
+        //se não falso
+        else
+            turning = false;
+
+        //se chegar no limite faz a rotação
+        if(turning)
+        {
+            Vector3 direction = myManager.transform.position - transform.position;
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                Quaternion.LookRotation(direction),
+                myManager.rotationSpeed * Time.deltaTime);
+        }
+        //se move
+        else
+        {
+            if (Random.Range(0, 100) < 10)
+                speed = Random.Range(myManager.minSpeed, myManager.maxSpeed);
+            if(Random.Range(0,100)<20)
+                ApplyRules();
+        }
+       
+        transform.Translate(0, 0, Time.deltaTime * speed);
+    }
+
+    void ApplyRules()
+    {
+        GameObject[] gos;
+        gos = myManager.allFish;
+
+        Vector3 vcentre = Vector3.zero;
+        Vector3 vavoid = Vector3.zero;
+        float gSpeed = 0.01f;
+        float nDistance;
+        int groupSize = 0;
+
+        foreach (GameObject go in gos)
+        {
+            if (go != this.gameObject)
+            {
+                nDistance = Vector3.Distance(go.transform.position, this.transform.position);
+                if(nDistance<= myManager.neighbourDistance)
+                {
+                    vcentre += go.transform.position;
+                    groupSize++;
+
+                    if(nDistance<1.0)
+                    {
+                        vavoid = vavoid + (this.transform.position - go.transform.position);
+                    }
+
+                    Flock anotherFlock = go.GetComponent<Flock>();
+                    gSpeed = gSpeed + anotherFlock.speed;
+                }
+            }
+
+        }
+        if(groupSize>0)
+        {
+            vcentre = vcentre / groupSize + (myManager.goalPOs - this.transform.position);
+            speed = gSpeed / groupSize;
+
+            Vector3 direction = (vcentre + vavoid) - transform.position;
+            if (direction != Vector3.zero)
+                transform.rotation = Quaternion.Slerp(transform.rotation,
+                    Quaternion.LookRotation(direction),
+                    myManager.rotationSpeed * Time.deltaTime);
+        }
+    }
+}
